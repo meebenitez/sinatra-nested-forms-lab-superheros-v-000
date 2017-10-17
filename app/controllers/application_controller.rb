bc43b789594d43ca4ff2939b4728e5663b726c57
Mary Benitez <meebenitez@gmail.com>
2017-10-17 06:10:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 require 'sinatra/base'
+require_relative '../models/hero.rb'
+require_relative '../models/team.rb'
+
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+  
 
 
 end
