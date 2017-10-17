5eb1597d0cc378ba1521bec14e9834c6495d9615
Mary Benitez <meebenitez@gmail.com>
2017-10-17 06:18:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,31 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :root
+    end
+
     get '/teams' do
       erb :team
     end
+
+
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+
 
 
 
